33e97ce0f90891cad4e79df0b924773e46a9e0fe
Will <wschuerm@gmail.com>
2022-11-07 16:48:50 -0800

    initial commit

---
+++ src/data/preprocess.py
@@ -0,0 +1,172 @@
+# %%
+#import libraries and modules
+import io
+import pandas as pd
+
+#Snorkel
+from snorkel.labeling import LabelingFunction
+import re
+from snorkel.preprocess import preprocessor
+from textblob import TextBlob
+from snorkel.labeling import PandasLFApplier
+from snorkel.labeling.model import LabelModel
+from snorkel.labeling import LFAnalysis
+from snorkel.labeling import filter_unlabeled_dataframe
+from snorkel.labeling import labeling_function
+
+#NLP packages
+import spacy
+from nltk.corpus import stopwords
+import string
+import nltk
+import nltk.tokenize
+punc = string.punctuation
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+
+#Supervised learning
+from tqdm import tqdm
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+##Deep learning libraries and APIs
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# %%
+# store the dataset as a Pandas Dataframe
+df = pd.read_csv('data/raw/abcnews-date-text.csv')
+#conduct some data cleaning
+# df = df.drop(['publish_date', 'Unnamed: 2'], axis=1)
+df = df.rename(columns = {'headline_text': 'text'})
+df['text'] = df['text'].astype(str)
+#check the data info
+df.info()
+
+# %%
+#define constants to represent the class labels :positive, negative, and abstain
+POSITIVE = 1
+NEGATIVE = 0
+ABSTAIN = -1
+#define function which looks into the input words to represent a proper label
+def keyword_lookup(x, keywords, label):  
+    if any(word in x.text.lower() for word in keywords):
+        return label
+    return ABSTAIN
+#define function which assigns a correct label
+def make_keyword_lf(keywords, label=POSITIVE):
+    return LabelingFunction(
+        name=f"keyword_{keywords[0]}",
+        f=keyword_lookup,
+        resources=dict(keywords=keywords, label=label))
+#resource: https://www.snorkel.org/use-cases/01-spam-tutorial#3-writing-more-labeling-functions
+#these two lists can be further extended 
+"""positive news might contain the following words' """
+keyword_positive = make_keyword_lf(keywords=['boosts', 'great', 'develops', 'promising', 'ambitious', 'delighted', 'record', 'win', 'breakthrough', 'recover', 'achievement', 'peace', 'party', 'hope', 'flourish', 'respect', 'partnership', 'champion', 'positive', 'happy', 'bright', 'confident', 'encouraged', 'perfect', 'complete', 'assured' ])
+"""negative news might contain the following words"""
+keyword_negative = make_keyword_lf(keywords=['war','solidiers', 'turmoil', 'injur','trouble', 'aggressive', 'killed', 'coup', 'evasion', 'strike', 'troops', 'dismisses', 'attacks', 'defeat', 'damage', 'dishonest', 'dead', 'fear', 'foul', 'fails', 'hostile', 'cuts', 'accusations', 'victims',  'death', 'unrest', 'fraud', 'dispute', 'destruction', 'battle', 'unhappy', 'bad', 'alarming', 'angry', 'anxious', 'dirty', 'pain', 'poison', 'unfair', 'unhealthy'
+                                              ], label=NEGATIVE)
+
+# %%
+#set up a preprocessor function to determine polarity & subjectivity using textlob pretrained classifier 
+@preprocessor(memoize=True)
+def textblob_sentiment(x):
+    scores = TextBlob(x.text)
+    x.polarity = scores.sentiment.polarity
+    x.subjectivity = scores.sentiment.subjectivity
+    return x
+#find polarity
+@labeling_function(pre=[textblob_sentiment])
+def textblob_polarity(x):
+    return POSITIVE if x.polarity > 0.6 else ABSTAIN
+#find subjectivity 
+@labeling_function(pre=[textblob_sentiment])
+def textblob_subjectivity(x):
+    return POSITIVE if x.subjectivity >= 0.5 else ABSTAIN
+
+# %%
+#combine all the labeling functions 
+lfs = [keyword_positive, keyword_negative, textblob_polarity, textblob_subjectivity ]
+#apply the lfs on the dataframe
+applier = PandasLFApplier(lfs=lfs)
+L_snorkel = applier.apply(df=df)
+#apply the label model
+label_model = LabelModel(cardinality=2, verbose=True)
+#fit on the data
+label_model.fit(L_snorkel)
+#predict and create the labels
+df["label"] = label_model.predict(L=L_snorkel)
+
+# %%
+#make a copy of the dataframe
+data = df.copy()
+#define a function which handles the text preprocessing 
+def preparation_text_data(data):
+    """
+    This pipeline prepares the text data, conducting the following steps:
+    1) Tokenization
+    2) Lemmatization
+    4) Removal of stopwords
+    5) Removal of punctuation
+    """
+    # initialize spacy object
+    nlp = spacy.load('en_core_web_sm')
+    # select raw text
+    raw_text = data.text.values.tolist()
+    # tokenize
+    tokenized_text = [[nlp(i.lower().strip())] for i in tqdm(raw_text)]
+    #define the punctuations and stop words
+    punc = string.punctuation 
+    stop_words = set(stopwords.words('english'))
+    #lemmatize, remove stopwords and punctuationd
+    corpus = []
+    for doc in tqdm(tokenized_text):
+        corpus.append([word.lemma_ for word in doc[0] if (word.lemma_ not in stop_words and word.lemma_ not in punc)])
+    # add prepared data to df
+    data["text"] = corpus
+    return data
+#apply the data preprocessing function
+data =  preparation_text_data(data)
+# save output
+data.to_csv('data/interim/abcnews_labeled.csv')
+
+# # %%
+# def text_representation(data):
+#   tfidf_vect = TfidfVectorizer()
+#   data['text'] = data['text'].apply(lambda text: " ".join(set(text)))
+#   X_tfidf = tfidf_vect.fit_transform(data['text'])
+#   print(X_tfidf.shape)
+#   print(tfidf_vect.get_feature_names())
+#   X_tfidf = pd.DataFrame(X_tfidf.toarray())
+#   return X_tfidf
+# #apply the TFIDV function
+# X_tfidf = text_representation(data)
+
+# # %%
+# X= X_tfidf
+# y = data['label']
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+# #fit Log Regression Model
+# clf= LogisticRegression()
+# clf.fit(X_train,y_train)
+# clf.score(X_test,y_test)
+# y_pred = clf.predict(X_test)
+# print(classification_report(y_test, y_pred))
+
+# # %%
+# new_data = ["The US imposes sanctions on Rassia because of the Ukranian war"]
+# tf = TfidfVectorizer()
+# tfdf = tf.fit_transform(data['text'])
+# vect = pd.DataFrame(tf.transform(new_data).toarray())
+# new_data = pd.DataFrame(vect)
+# logistic_prediction = clf.predict(new_data)
+# print(logistic_prediction)
+
+# # %%
+# new_data.to_csv('data/interim/abcnews_labelled')
+
+
